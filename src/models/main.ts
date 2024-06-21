6d9b9c2d55be7b1466171778ebe2b1a14e787714
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2024-06-21 16:10:54 -0400

    Refactor user authentication logic
    Moved user existence check and creation logic into new User and Session controllers. This has resulted in a cleaner codebase and improved maintainability of the user authentication process. Code deletion was done from sign-in related functionalities where duplicate functionality was performed and redundant code files were removed. On the other hand, related documentation was added to the stripe.ts file for clarity.

---
+++ src/models/main.ts
@@ -0,0 +1,132 @@
+export interface UserType {
+  ADMIN: 'ADMIN';
+  EMPLOYEE: 'EMPLOYEE';
+  OWNER: 'OWNER';
+  GUEST: 'GUEST';
+}
+
+export interface Account {
+  userId: string;
+  type: string;
+  provider: string;
+  providerAccountId: string;
+  refresh_token?: string;
+  access_token?: string;
+  expires_at?: number;
+  token_type?: string;
+  scope?: string;
+  id_token?: string;
+  session_state?: string;
+  createdAt: Date;
+  updatedAt: Date;
+  user: User;
+}
+
+export interface Session {
+  sessionToken: string;
+  userId: string;
+  expires: number;
+  user: User;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface VerificationToken {
+  identifier: string;
+  token: string;
+  expires: Date;
+}
+
+export interface Authenticator {
+  credentialID: string;
+  userId: string;
+  providerAccountId: string;
+  credentialPublicKey: string;
+  counter: number;
+  credentialDeviceType: string;
+  credentialBackedUp: boolean;
+  transports?: string;
+  user: User;
+}
+
+export interface User {
+  id: string;
+  name?: string;
+  email?: string;
+  emailVerified?: Date;
+  image?: string;
+  accounts: Account[];
+  sessions: Session[];
+  Authenticator: Authenticator[];
+  stripeCustomerId?: string;
+  isActive: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  restaurants: Restaurant[];
+  role: UserType;
+}
+
+export interface Restaurant {
+  id: string;
+  name: string;
+  image?: string;
+  menu?: RestaurantMenu;
+  location?: Location;
+  restaurantMenuId: string;
+  previousAvgOrderCount: number;
+  newAvgOrderCount: number;
+  tables: Table[];
+  Orders: Orders[];
+  User?: User;
+  userId?: string;
+}
+
+export interface RestaurantMenu {
+  id: string;
+  menuItems: MenuItems[];
+  Restaurant: Restaurant[];
+}
+
+export interface Location {
+  id: string;
+  latitude?: number;
+  longitud?: number;
+  coordinates?: string;
+  restaurant: Restaurant;
+  restaurandId: string;
+}
+
+export interface Table {
+  id: string;
+  tableNumber: number;
+  Restaurant?: Restaurant;
+  restaurantId?: string;
+}
+
+export interface MenuItems {
+  id: string;
+  title: string;
+  description: string;
+  image?: string;
+  availability: boolean;
+  category: Categories;
+  price: number;
+  categoriesId: string;
+  RestaurantMenu: RestaurantMenu;
+  restaurantMenuId: string;
+  orders: Orders[];
+}
+
+export interface Categories {
+  id: string;
+  title: string;
+  MenuItems: MenuItems[];
+}
+
+export interface Orders {
+  id: string;
+  menuItems: MenuItems[];
+  subTotal: number;
+  restaurant: Restaurant;
+  restaurantId: string;
+}
--- src/server/routers/stripe.ts
+++ src/server/routers/stripe.ts
@@ -1,8 +1,33 @@
+/**
+ * The `stripeRouter` is a tRPC-router instance exposing a single `checkout` mutation.
+ * It is used to create a Stripe checkout session for a user that has a stripeCustomerId.
+ * @module stripeRouter
+ */
+
 import { authedProcedure, router } from '../trpc';
 import { TRPCError } from '@trpc/server';
 import Stripe from 'stripe';
 
+/**
+ * The `stripeRouter` object exports a tRPC-router configuration that includes a `checkout` mutation.
+ * This mutation creates a Stripe checkout session for a user that has a stripeCustomerId associated with their account.
+ * If the user is not authenticated, or the user does not exist, the mutation will throw an error.
+ * The mutation will also throw an error if it is unable to create the checkout session.
+ */
 export const stripeRouter = router({
+  /**
+   * The `checkout` mutation creates a Stripe checkout session for a user that has a stripeCustomerId.
+   * - If the user is not authenticated or the stripeCustomerId is not set, the mutation throws an error.
+   * - The mutation throws an error if it cannot create a session.
+   * @function checkout
+   * @async
+   * @param {Object} context - The tRPC context object.
+   * @property {Object} ctx.session - The session object.
+   * @throws {TRPCError} - If user is not registered as a customer
+   * @throws {TRPCError} - If user does not exist
+   * @throws {TRPCError} - If unable to create an invoice session
+   * @returns {Object} Response object with status, message, and session result.
+   */
   checkout: authedProcedure.mutation(async ({ ctx }) => {
     const session = ctx.session;
     if (!session?.user?.stripeCustomerId) {
