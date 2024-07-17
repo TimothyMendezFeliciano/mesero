a09a3843cf5a1c8ca27ef07f65b21b51185c5cb5
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2024-07-17 14:27:31 -0400

    delete dist

--- dist/constants/companyInfo.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.COMPANY_INFO = void 0;
-exports.COMPANY_INFO = {
-    name: 'MeseroPR',
-    tagline: 'QR Codes that work for you.',
-    description: 'MeseroPR is designed to help manage your orders with a simple QR Code.',
-};
--- dist/models/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// export interface UserType {
-//   ADMIN: 'ADMIN';
-//   EMPLOYEE: 'EMPLOYEE';
-//   OWNER: 'OWNER';
-//   GUEST: 'GUEST';
-// }
-Object.defineProperty(exports, "__esModule", { value: true });
