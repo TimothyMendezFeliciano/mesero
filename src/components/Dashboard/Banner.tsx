32deae4bcebbe7bbc31fe690e1242d49e683258d
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2024-07-22 22:10:13 -0400

    DashboardBanner mocked

---
+++ src/components/Dashboard/Banner.tsx
@@ -0,0 +1,131 @@
+import { useState } from 'react';
+import { PlusCircleIcon } from '@heroicons/react/24/outline';
+import { User } from '../../models/main';
+
+type DashboardBannerType = {
+  admin: User;
+};
+export default function DashboardBanner({ admin }: DashboardBannerType) {
+  const restaurants = [
+    { id: 1, name: 'Sanwicheros de la Plaza' },
+    { id: 2, name: 'Combinaciones Chinas' },
+    { id: 3, name: 'Restaurante 3' },
+  ];
+
+  const [selected, setSelected] = useState(restaurants[0]);
+
+  return (
+    <div className={'flex flex-row justify-between items-center'}>
+      <div className={'inline-flex gap-4'}>
+        <select
+          className={
+            'select select-bordered w-full max-w-xs select-primary truncate'
+          }
+        >
+          <option disabled selected>
+            Select Restaurant Timesheet
+          </option>
+          {restaurants.map((restaurant) => (
+            <option key={restaurant.id}>{restaurant.name}</option>
+          ))}
+        </select>
+
+        <button className={'btn btn-secondary'}>
+          <PlusCircleIcon className={'w-10 h-10'} />
+        </button>
+      </div>
+      <div className='avatar'>
+        <div className='w-24 rounded'>
+          <img src={admin.image} />
+          {/*<img src='https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp' />*/}
+        </div>
+      </div>
+      {/*<div>*/}
+      {/*  <UserIcon className={'w-10 h-10'} />*/}
+      {/*</div>*/}
+    </div>
+  );
+
+  // return (
+  //   <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+  //     <UserIcon className="w-10 h-10" />
+  //     <div className="flex-1 flex items-center justify-center ">
+  //       <Listbox value={selected} onChange={setSelected}>
+  //         {({ open }) => (
+  //           <>
+  //             <Listbox.Label className="block text-sm font-medium text-gray-700">
+  //               Selections
+  //             </Listbox.Label>
+  //             <div className="mt-1 relative">
+  //               <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg border border-gray-300 shadow-sm cursor-default focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
+  //                 <span className="block truncate">{selected.name}</span>
+  //                 <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+  //                   <ArrowDownIcon
+  //                     className="h-5 w-5 text-gray-400"
+  //                     aria-hidden="true"
+  //                   />
+  //                 </span>
+  //               </Listbox.Button>
+  //
+  //               <Transition
+  //                 show={open}
+  //                 as="div"
+  //                 className="absolute mt-1 w-full rounded-md bg-white shadow-lg"
+  //                 leave="transition ease-in duration-100"
+  //                 leaveFrom="opacity-100"
+  //                 leaveTo="opacity-0"
+  //               >
+  //                 <Listbox.Options
+  //                   static
+  //                   className="py-1 rounded-md shadow-xs max-h-60 ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm"
+  //                 >
+  //                   {people.map((person) => (
+  //                     <Listbox.Option
+  //                       key={person.id}
+  //                       className={({ active }) =>
+  //                         `${
+  //                           active
+  //                             ? 'text-indigo-900 bg-indigo-100'
+  //                             : 'text-gray-900'
+  //                         } cursor-default select-none relative py-2 pl-10 pr-4`
+  //                       }
+  //                       value={person}
+  //                     >
+  //                       {({ selected, active }) => (
+  //                         <>
+  //                           <span
+  //                             className={`${
+  //                               selected ? 'font-medium' : 'font-normal'
+  //                             } block truncate`}
+  //                           >
+  //                             {person.name}
+  //                           </span>
+  //                           {selected && (
+  //                             <span
+  //                               className={`${
+  //                                 active ? 'text-indigo-600' : 'text-indigo-600'
+  //                               } absolute inset-y-0 left-0 flex items-center pl-3`}
+  //                             >
+  //                               <CheckIcon
+  //                                 className="h-5 w-5"
+  //                                 aria-hidden="true"
+  //                               />
+  //                             </span>
+  //                           )}
+  //                         </>
+  //                       )}
+  //                     </Listbox.Option>
+  //                   ))}
+  //                 </Listbox.Options>
+  //               </Transition>
+  //             </div>
+  //           </>
+  //         )}
+  //       </Listbox>
+  //     </div>
+  //     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  //       Click me
+  //     </button>
+  //   </div>
+  // );
+}
--- src/pages/dashboard/admin.tsx
+++ src/pages/dashboard/admin.tsx
@@ -1,48 +1,46 @@
-import { NextPage } from 'next';
-import { useSession } from 'next-auth/react';
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+  NextPage,
+} from 'next';
 import { requireAuth } from '../../server/requireAuth';
 import Layout from '../../components/Layout';
 import DashboardLayout from '../../components/Layout/DashboardLayout';
+import DashboardBanner from '../../components/Dashboard/Banner';
+import { getServerSession, Session } from 'next-auth';
+import { nextAuthOptions } from '../api/auth/[...nextauth]';
+import { useMemo } from 'react';
+import { User } from '../../models/main';
 
-export const getServerSideProps = requireAuth(async () => {
-  return { props: {} };
-});
+export const getServerSideProps: GetServerSideProps = requireAuth(
+  async (ctx: GetServerSidePropsContext) => {
+    const session1: Session | null = await getServerSession(
+      ctx.req,
+      ctx.res,
+      nextAuthOptions,
+    );
+    return {
+      props: {
+        session: JSON.stringify(session1),
+      },
+    };
+  },
+);
 
-const Admin: NextPage = () => {
-  const { data } = useSession();
+const Admin: NextPage = (props, context) => {
+  const session: Session = useMemo(
+    () => JSON.parse(props.session),
+    [props.session],
+  );
+
   return (
     <Layout>
       <DashboardLayout
-        TopComponent={<h1>This is a Banner</h1>}
+        TopComponent={<DashboardBanner admin={session.user as User} />}
         LeftComponent={<h1>This is a Left Component</h1>}
         MainComponent={<h1>This is a Main Component</h1>}
       />
-      {/*<div className="hero min-h-screen bg-base-200">*/}
-      {/*  <div className="hero-content">*/}
-      {/*    <div className="max-w-lg">*/}
-      {/*      <h1 className="text-5xl text-center font-bold leading-snug text-gray-400">*/}
-      {/*        Te has registrado!*/}
-      {/*      </h1>*/}
-      {/*      <p className="my-4 text-center leading-loose">*/}
-      {/*        Has logrado accesar esta pagina por que te registrastes. De lo*/}
-      {/*        contrario, serias dirigido hacia la pagina inicial.*/}
-      {/*      </p>*/}
-      {/*      <div className="my-4 bg-gray-700 rounded-lg p-4">*/}
-      {/*        <pre>*/}
-      {/*          <code>{JSON.stringify(data, null, 2)}</code>*/}
-      {/*        </pre>*/}
-      {/*      </div>*/}
-      {/*      <div className="text-center">*/}
-      {/*        <button*/}
-      {/*          className="btn btn-secondary"*/}
-      {/*          onClick={() => signOut({ callbackUrl: '/' })}*/}
-      {/*        >*/}
-      {/*          Logout*/}
-      {/*        </button>*/}
-      {/*      </div>*/}
-      {/*    </div>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
     </Layout>
   );
 };
