cf6805b692db518aaf4d67335aaaa807db0b1f63
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2024-10-02 21:03:41 -0400

    lint fix

--- src/hooks/useModalContext.ts
+++ src/hooks/useModalContext.ts
@@ -2,11 +2,11 @@ import { useContext } from 'react';
 import { ModalContext, ModalContextType } from '../context/ModalContext';
 
 export function useModalContext() {
-  const context = useContext<ModalContextType>(ModalContext)
+  const context = useContext<ModalContextType>(ModalContext);
 
-  if(!context) {
+  if (!context) {
     throw new Error('useModalContext must be used within a ModalProvider');
   }
 
-  return context
+  return context;
 }
